2cab91cea40f7fcdc64654fc1b9c225b500f854b
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-05-06 23:36:33 -0700

    feat(card-comps): created component for data cards, added styles in css and BStrp, used images for BG

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,21 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import  GotCards  from '../components/GotCards';
 import axios from 'axios';
 
 const Homepage = () => {
-    const [books, setBooks] = useState('');
+    const [books, setBooks] = useState([]);
 
     const getBooks = () => {
-        axios.get("https://www.anapioficeandfire.com/api/books").then(
-          (response) => {
-          console.log(response);
-          setBooks(response.data.name + " ... " + response.data.name);
-        });
+        axios.get("https://www.anapioficeandfire.com/api/books")
+        .then(response => {
+          console.log(response.data)
+          setBooks(response.data);
+          if (!response.ok) {
+            throw Error('ERROR');
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log(data.data); 
+        })
+
+        .catch(error => {
+          console.log(error);
+        })
       };
+
+      
     
       return (
-        <div className="col-sm-12 text-center">
-          <h1>Game of Thrones</h1>
-          <button onClick={getBooks}>G.O.T. Books</button>
+        
+        <div className="container text-center my-5">
+          <div className="col-sm-12 mt-3">
+          <h1 className='header'>Game of Thrones</h1>
+          <button className='bg-info' onClick={getBooks}>G.O.T. Books</button>
+
+          <GotCards books={books}/>
+          </div>
+
         </div>
       );
     
